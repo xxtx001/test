7e655c03d0c19c7a83337cd30d17b13cf5930314
Yingpeng Xu <YpXu@bhs-world.com>
2022-03-21 10:16:10 +0800

    update for review git command

--- hello.c
+++ hello.c
@@ -6,4 +6,5 @@ void main()
 	printf("this is a new added line, 17-11-2021\n");
 	printf("hello world!\n");	
 	printf("yes! here is the branch info\n");
+	printf("update this line in 21th.march 2022\n");
 }
